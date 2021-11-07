e68554b3281662884f28945d31a049aaead91045
yohamta <yohamta@gmail.com>
2021-11-07 16:26:50 +0900

    Add draw functions

--- draw.go
+++ draw.go
@@ -13,6 +13,15 @@ func DrawSprite(screen *ebiten.Image, spr *Sprite, index int, x, y, rot, sx, sy,
 	spr.Draw(screen, index, drawOpts)
 }
 
+// DrawSpriteWithOpts draws a sprite to the screen.
+func DrawSpriteWithOpts(screen *ebiten.Image, spr *Sprite, index int, opts *DrawOptions, shaderOpts *ShaderOptions) {
+	if shaderOpts != nil {
+		spr.DrawWithShader(screen, index, opts, shaderOpts)
+	} else {
+		spr.Draw(screen, index, opts)
+	}
+}
+
 // DrawAnime draws an animation to the screen.
 func DrawAnime(screen *ebiten.Image, anim *Animation, x, y, rot, sx, sy, ox, oy float64) {
 	drawOpts.SetPos(x, y)
@@ -20,4 +29,13 @@ func DrawAnime(screen *ebiten.Image, anim *Animation, x, y, rot, sx, sy, ox, oy 
 	drawOpts.SetScale(sx, sy)
 	drawOpts.SetOrigin(ox, oy)
 	anim.Draw(screen, drawOpts)
+}
+
+// DrawAnimeWithOpts draws an anime to the screen.
+func DrawAnimeWithOpts(screen *ebiten.Image, anim *Animation, index int, opts *DrawOptions, shaderOpts *ShaderOptions) {
+	if shaderOpts != nil {
+		anim.DrawWithShader(screen, opts, shaderOpts)
+	} else {
+		anim.Draw(screen, opts)
+	}
 }

26dfd4477dab2c62d1c372bc40ada2294a9ad467
yohamta <yohamta@gmail.com>
2022-08-08 20:41:21 +0900

    Update exmaple

--- examples/simple/main.go
+++ examples/simple/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"embed"
 	"image"
 	_ "image/png"
 	"log"
@@ -10,7 +11,6 @@ import (
 	"github.com/hajimehoshi/ebiten/v2"
 
 	"github.com/yohamta/ganim8/v2"
-	"github.com/yohamta/ganim8/v2/examples/assets/images"
 )
 
 const (
@@ -19,52 +19,50 @@ const (
 )
 
 type Game struct {
-	prevUpdateTime time.Time
-	anim           *ganim8.Animation
+	prev      time.Time
+	animation *ganim8.Animation
 }
 
-var (
-	monsterImage = ebiten.NewImageFromImage(bytes2Image(&images.CHARACTER_MONSTER_SLIME_BLUE))
-)
+func NewGame() *Game {
+	g := &Game{
+		prev: time.Now(),
+	}
+	g.setupMonsterAnimation()
+
+	return g
+}
 
 func (g *Game) Update() error {
-	elapsedTime := time.Now().Sub(g.prevUpdateTime)
+	elapsedTime := time.Now().Sub(g.prev)
+	g.prev = time.Now()
 
-	g.anim.Update(elapsedTime)
+	g.animation.Update(elapsedTime)
 
-	g.prevUpdateTime = time.Now()
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
 	screen.Clear()
-	g.anim.Draw(screen, ganim8.DrawOpts(screenWidth/2, screenHeight/2, 0, 1, 1, 0.5, 0.5))
+	g.animation.Draw(screen, ganim8.DrawOpts(screenWidth/2, screenHeight/2, 0, 1, 1, 0.5, 0.5))
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
 	return screenWidth, screenHeight
 }
 
-func NewGame() *Game {
-	g := &Game{
-		prevUpdateTime: time.Now(),
-	}
-	g.setupAnimations()
-
-	return g
-}
-
-func (g *Game) setupAnimations() {
+func (g *Game) setupMonsterAnimation() {
 	grid := ganim8.NewGrid(100, 100, 500, 600)
-	sprite := ganim8.NewSprite(monsterImage, grid.GetFrames("1-5", 5))
-	g.anim = ganim8.NewAnimation(sprite, 100*time.Millisecond, ganim8.Nop)
+	img := ebiten.NewImageFromImage(readImage("assets/monster.png"))
+	sprite := ganim8.NewSprite(img, grid.GetFrames("1-5", 5))
+	g.animation = ganim8.NewAnimation(sprite, 100*time.Millisecond, ganim8.Nop)
 }
 
-func main() {
-	ebiten.SetWindowSize(screenWidth, screenHeight)
-	if err := ebiten.RunGame(NewGame()); err != nil {
-		log.Fatal(err)
-	}
+//go:embed assets/*
+var assets embed.FS
+
+func readImage(file string) image.Image {
+	b, _ := assets.ReadFile(file)
+	return bytes2Image(&b)
 }
 
 func bytes2Image(rawImage *[]byte) image.Image {
@@ -73,4 +71,11 @@ func bytes2Image(rawImage *[]byte) image.Image {
 		log.Fatal("Bytes2Image Failed: ", format, error)
 	}
 	return img
+}
+
+func main() {
+	ebiten.SetWindowSize(screenWidth, screenHeight)
+	if err := ebiten.RunGame(NewGame()); err != nil {
+		log.Fatal(err)
+	}
 }
